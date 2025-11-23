dfaef6c797e7dd0c02781b29bf40b815028ceaa8
Roger Reis <roger.gdreis@gmail.com>
2025-11-22 21:17:23 -0300

    2025-11-22-21:17

---
+++ aula022.py
@@ -0,0 +1,11 @@
+# Operadores lógicos
+# and (e) or (ou) not(não)
+# or - Qualquer condição verdadeira avalia
+# toda a expressão como verdadeira.
+# Se qualquer valor for considerado verdadeiro,
+# a expressão inteira será avaliada naquele valor.
+# São considerados falsy:
+# 0 0.0 '' False.
+# Também existe o tipo None que é
+# usado para representar um não valor
+
