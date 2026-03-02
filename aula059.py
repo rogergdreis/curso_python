125a8ec79aa8096160042c410beb4ca7bc4091bb
Roger Reis <roger.gdreis@gmail.com>
2026-03-01 21:19:01 -0300

    2026-03-01

---
+++ aula059.py
@@ -0,0 +1,11 @@
+# Desempacotamento em camadas
+# de métodos e funções
+string = 'ABCD'
+lista = ['Maria', 'Helena', 'Eduarda']
+tupla = 'Python', 'é', 'legal'
+
+a, b, c = lista
+print(a, c)
+
+for nome in lista:
+    print(nome)
